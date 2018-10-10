949faa399829bff88bf6b401a2838c64189684e3
christopher mochizuki <mochic808@gmail.com>
2018-10-10 12:49:11 -0700

    fix test

--- react-client/src/components/headerComponent/__tests__/NavBar.test.jsx
+++ react-client/src/components/headerComponent/__tests__/NavBar.test.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
 import NavBar from '../NavBar';
 import renderer from 'react-test-renderer';
 
 it('renders deep', () => {
-  const tree = renderer.create(<NavBar />);
+  const tree = renderer.create(
+    <MemoryRouter>
+      <NavBar />
+    </MemoryRouter>
+  );
   expect(tree).toMatchSnapshot();
 });
